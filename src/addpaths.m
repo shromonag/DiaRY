08ddcab3579d820b586be75fc73115ec01463ac8
Shromona MacBook <shromona.ghosh@berkeley.edu>
2016-01-22 15:20:43 -0800

    addpaths

---
+++ src/addpaths.m
@@ -0,0 +1,9 @@
+%% Adding all the required files
+
+% Add the files necessary to run the tool
+addpath('./YalmipFiles/');
+
+% Add the files necessary for running test cases in HSCC2016
+addpath(genpath('../BluSTLFiles/'));
+addpath(genpath('../CrPrsTLFiles/'));
+addpath(genpath('../HSCC2016/'));
