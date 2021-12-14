8de7611a04a72d2473f92276f8681d1344ff2e31
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 02:09:33 +0800

    Delete src directory

--- src/writeConstraintsFile.m
+++
@@ -1,9 +0,0 @@
-%% This function updates the model with values form the adversary
-
-function status = writeConstraintsFile(varNames, varVals)
-    fileID = fopen('./InterfaceFiles/UpdateMILPwithCons.txt', 'w');
-    fprintf(fileID, '%d %d\n', [varNames; varVals]);
-    fclose(fileID);
-
-    [status, result] = unix('python ./PythonFiles/UpdateMILPwithCons.py');
-end
